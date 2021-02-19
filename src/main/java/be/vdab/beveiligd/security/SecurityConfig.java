a7237424c3e30a9e1cfa8c4683302121ac698c05
Arne <a.vaneycken@gmail.com>
2021-02-19 10:18:13 +0100

    5.17 Database -> Parameter 0 of constructor in be.vdab.beveiligd.security.SecurityConfig required a bean of type 'javax.sql.DataSource' that could not be found.

--- src/main/java/be/vdab/beveiligd/security/SecurityConfig.java
+++ src/main/java/be/vdab/beveiligd/security/SecurityConfig.java
@@ -1,27 +1,39 @@
 package be.vdab.beveiligd.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
 /**
  * @author Arne Van Eycken
  * @version 1.0
  */
+
 
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String MANAGER = "manager";
     private static final String HELPDESKMEDEWERKER = "helpdeskmedewerker";
     private static final String MAGAZIJNIER = "magazijnier";
+    private final DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("joe").password("{noop}theboss").authorities(MANAGER).and()
-                .withUser("averell").password("{noop}hungry").authorities(HELPDESKMEDEWERKER, MAGAZIJNIER);
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
